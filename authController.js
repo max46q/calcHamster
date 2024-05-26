a70e5bdf2eaded3b3a06666b8c23d2f66239cf0f
max46q <2000maksym2000@gmail.com>
2024-05-27 00:55:08 +0300

    end post and get method and middleware

--- authController.js
+++ authController.js
@@ -1,10 +1,31 @@
 const User = require("./models/User");
 const Role = require("./models/Role");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { secret } = require("./config");
+
+const { validationResult } = require("express-validator");
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, {
+    expiresIn: "3h",
+  });
+};
 
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: "Помилка при регістрації", errors: errors.array() });
+      }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
       if (candidate) {
@@ -29,6 +50,19 @@ class authController {
 
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Користувача з таким іменем не існує" });
+      }
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Неправильний пароль" });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (err) {
       console.log(err);
       res.status(400).json({ message: "Login failed" });
@@ -37,7 +71,8 @@ class authController {
 
   async getCards(req, res) {
     try {
-      res.json("server worked");
+      const users = await User.find();
+      res.json(users);
     } catch (err) {
       console.log(err);
     }
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,24 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("./authController");
+const { check } = require("express-validator");
+const authMiddleware = require("./middlewaree/authMiddleware");
+const roleMiddleware = require("./middlewaree/roleMiddleware");
 
-router.post("/registrations", controller.registration);
+router.post(
+  "/registrations",
+  [
+    check("username", "Логін повинен бути мінімум з 4 символів").isLength({
+      min: 4,
+    }),
+    check(
+      "password",
+      "Пароль повинен складати не менше 6 символів і не більше 12"
+    ).isLength({ min: 6, max: 12 }),
+  ],
+  controller.registration
+);
 router.post("/login", controller.login);
-router.get("/cards", controller.getCards);
+router.get("/cards", authMiddleware, controller.getCards);
 
 module.exports = router;
