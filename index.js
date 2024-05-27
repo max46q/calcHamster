92aed1efacac8aec3cb503972761b47b3f83053c
max46q <2000maksym2000@gmail.com>
2024-05-27 21:48:40 +0300

    надіюсь не не наєбанется vecel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
+const ejs = require("ejs");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,23 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use("/auth", authRouter);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/views"));
+app.use(express.static("public"));
+const bodyParser = require("body-parser");
+app.engine("ejs", require("ejs").renderFile);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/namek", (req, res) => {
+  res.render("namek");
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const start = async () => {
   try {
